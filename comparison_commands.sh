422a8ca353b72ec139055189df1e573c903c1b92
vortacs <jtoscanifield@ucmerced.edu>
2019-02-28 07:23:34 -0800

    potential fixes?

--- comparison_commands.sh
+++ comparison_commands.sh
@@ -9,281 +9,285 @@ PHY_COMPARE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 source $1
 
-REP_TEMPFILE=/tmp/rep_temp.tmp
-echo 0 > $REP_TEMPFILE
+# REP_TEMPFILE=/tmp/rep_temp.tmp
+# echo 0 > $REP_TEMPFILE
+#
+# for ((n=0;n<$REPLICATES;n++)); do
+#
+#   REP_COUNTER=$[$(cat $REP_TEMPFILE) + 1]
 
-for ((n=0;n<$REPLICATES;n++)); do
+mkdir -p $outdir
 
-  REP_COUNTER=$[$(cat $REP_TEMPFILE) + 1]
+maind=$(pwd)
 
-  mkdir -p $outdir
+cd $outdir
 
-  maind=$(pwd)
+workd=$(pwd)
 
-  cd $outdir
+ls ${master_reads}/*$r1_tail | split -a 20 -l $file_split_num
 
-  workd=$(pwd)
+# potential randomization command
+#ls ${master_reads}/*$r1_tail | sort -R | split -a 20 -l $file_split_num
 
-  ls ${master_reads}/*$r1_tail | split -a 20 -l $file_split_num
+wait
 
-  # potential randomization command
-  #ls ${master_reads}/*$r1_tail | sort -R | split -a 20 -l $file_split_num
-
-  wait
-
-  for j in $(ls xa*); do
-    mkdir $j-read-dir
-    for i in $(cat $j); do
-      cp ${read_dir}/$i $j-read-dir/
-      cp ${read_dir}/${i%$r1_tail}$r2_tail $j-read-dir/
-      wait
-    done
+for j in $(ls xa*); do
+  mkdir $j-read-dir
+  for i in $(cat $j); do
+    cp ${read_dir}/$i $j-read-dir/
+    cp ${read_dir}/${i%$r1_tail}$r2_tail $j-read-dir/
     wait
   done
-
-  first_tree_dir=$(ls -d */ | head -1)
-
-  second_tree_dir=$(ls -d */ | head -2 | tail -1)
-
-  printf "$first_tree_dir"
-
-  printf "$second_tree_dir"
-
   wait
+done
 
-  cat <<EOF > first_tree_assembly.cfg
-  ## Config file for gon_phyling pipeline
-  # change the values of the variables to control the pipeline
-  # the variables are structured like this:
-  # variable_name="value_of_the_variable"
-  # value of the variable can be a path to a file or directory of files
+first_tree_dir=$(ls -d */ | head -1)
 
-  # Path to the reference genome required for Parsnp
-  ref_genome="/path/to/reference/genome"
+second_tree_dir=$(ls -d */ | head -2 | tail -1)
 
-  # Path to the directory of reads for assembly
-  read_dir="$workd/$first_tree_dir"
+printf "$first_tree_dir"
 
-  # number of threads for Spades assembly and RAxML inference
-  threads="4"
+printf "$second_tree_dir"
 
-  # File stubs required to assemble paired end reads
-  r1_tail="R1.fastq"
-  r2_tail="R2.fastq"
+wait
+
+cat <<EOF > first_tree_assembly.cfg
+## Config file for gon_phyling pipeline
+# change the values of the variables to control the pipeline
+# the variables are structured like this:
+# variable_name="value_of_the_variable"
+# value of the variable can be a path to a file or directory of files
+
+# Path to the reference genome required for Parsnp
+ref_genome="/path/to/reference/genome"
+
+# Path to the directory of reads for assembly
+read_dir="$workd/$first_tree_dir"
+
+# number of threads for Spades assembly and RAxML inference
+threads="4"
+
+# File stubs required to assemble paired end reads
+r1_tail="R1.fastq"
+r2_tail="R2.fastq"
 EOF
 
-  $phycorder_path/gon_phyling.sh ./first_tree_assembly.cfg
+$phycorder_path/gon_phyling.sh ./first_tree_assembly.cfg
 
-  mkdir updated_phycorder_required_files
+mkdir updated_phycorder_required_files
 
-  cp $workd/$first_tree_dir/trimmed_reads/spades_output/genomes_for_parsnp/alignment_fixing/combo.fas ./updated_phycorder_required_files/
+cp $workd/$first_tree_dir/trimmed_reads/spades_output/genomes_for_parsnp/alignment_fixing/combo.fas ./updated_phycorder_required_files/
 
-  cp $workd/$first_tree_dir/trimmed_reads/spades_output/genomes_for_parsnp/alignment_fixing/RAxML_bestTree.core_genome_run.out ./updated_phycorder_required_files/
+cp $workd/$first_tree_dir/trimmed_reads/spades_output/genomes_for_parsnp/alignment_fixing/RAxML_bestTree.core_genome_run.out ./updated_phycorder_required_files/
 
-  wait
+wait
 
-  for dir in $(ls -d */ ); do
-    # printf $dir
-    # printf $first_tree_dir
-    if [ "$dir" == "$first_tree_dir" ]; then
+for dir in $(ls -d */ ); do
+  # printf $dir
+  # printf $first_tree_dir
+  if [ "$dir" == "$first_tree_dir" ]; then
 
-      printf "skipping first first tree file"
+    printf "skipping first first tree file"
 
-    elif [ "$dir" == "$second_tree_dir" ]; then
+  elif [ "$dir" == "$second_tree_dir" ]; then
 
-      # cat <<phy_loop > $dir_phycorder_run.cfg
-      cat <<phy_loop > basic.cfg
+    # cat <<phy_loop > $dir_phycorder_run.cfg
+    cat <<phy_loop > basic.cfg
 
-      ## Welcome to the Phycorder config file
-      # Change the variable values to match the files and numbers you wish to use
-      # this is the path to the phycorder directory. Dont move it.
+    ## Welcome to the Phycorder config file
+    # Change the variable values to match the files and numbers you wish to use
+    # this is the path to the phycorder directory. Dont move it.
 
-      # PHYCORDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+    # PHYCORDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-      # the path to the previously generated alignment file
+    # the path to the previously generated alignment file
 
-      align="$workd/updated_phycorder_required_files/combo.fas"
+    align="$workd/updated_phycorder_required_files/combo.fas"
 
-      # the path to the previously generated tree file made from the alignment file
+    # the path to the previously generated tree file made from the alignment file
 
-      tree="$workd/updated_phycorder_required_files/RAxML_bestTree.core_genome_run.out"
+    tree="$workd/updated_phycorder_required_files/RAxML_bestTree.core_genome_run.out"
 
-      # the directory of paired end read pairs you belonging to taxa you wish to add to your phylogeny
+    # the directory of paired end read pairs you belonging to taxa you wish to add to your phylogeny
 
-      read_dir="$workd/$dir"
+    read_dir="$workd/$dir"
 
-      # the number of taxa that can be added to the phylogeny at a single time
-      # should be less than the number of cores available
-      # should be balanced with the number of threads you will assign to the programs within phycorder
+    # the number of taxa that can be added to the phylogeny at a single time
+    # should be less than the number of cores available
+    # should be balanced with the number of threads you will assign to the programs within phycorder
 
-      phycorder_runs="2"
+    phycorder_runs="2"
 
-      # the number of threads you wish to make available to each phycorder run
-      # for mapping with bowtie and inference with RAxML
+    # the number of threads you wish to make available to each phycorder run
+    # for mapping with bowtie and inference with RAxML
 
-      threads="2"
+    threads="2"
 
-      # the tail identifiers of the read pairs
-      # if the full read name is "Clade_1_01_R1_001.fastq" and Clade_1_01_R2_001.fastq"
-      # then only put the portion of the file names that change to correspond to the read pairs
-      # in this example, Clade_1_01_ identify the taxons and so must not be included
+    # the tail identifiers of the read pairs
+    # if the full read name is "Clade_1_01_R1_001.fastq" and Clade_1_01_R2_001.fastq"
+    # then only put the portion of the file names that change to correspond to the read pairs
+    # in this example, Clade_1_01_ identify the taxons and so must not be included
 
-      r1_tail="R1.fastq"
-      r2_tail="R2.fastq"
+    r1_tail="R1.fastq"
+    r2_tail="R2.fastq"
 
-      # the output directory for your final information
+    # the output directory for your final information
 
-      outdir="$workd/$dir/phycorder-out"
-
-phy_loop
-
-      wait
-
-      time $phycorder_path/multi_map.sh ./basic.cfg
-
-      cp $workd/$dir/phycorder-out/combine_and_infer/RAxML_bestTree.consensusFULL ./updated_phycorder_required_files/
-
-      cp $workd/$dir/phycorder-out/combine_and_infer/extended.aln ./updated_phycorder_required_files/
-
-      wait
-    else
-
-      cat <<phy_loop > basic.cfg
-
-      ## Welcome to the Phycorder config file
-      # Change the variable values to match the files and numbers you wish to use
-      # this is the path to the phycorder directory. Dont move it.
-
-      # PHYCORDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-      # the path to the previously generated alignment file
-
-      align="$workd/updated_phycorder_required_files/extended.aln"
-
-      # the path to the previously generated tree file made from the alignment file
-
-      tree="$workd/updated_phycorder_required_files/RAxML_bestTree.consensusFULL"
-
-      # the directory of paired end read pairs you belonging to taxa you wish to add to your phylogeny
-
-      read_dir="$workd/$dir"
-
-      # the number of taxa that can be added to the phylogeny at a single time
-      # should be less than the number of cores available
-      # should be balanced with the number of threads you will assign to the programs within phycorder
-
-      phycorder_runs="2"
-
-      # the number of threads you wish to make available to each phycorder run
-      # for mapping with bowtie and inference with RAxML
-
-      threads="2"
-
-      # the tail identifiers of the read pairs
-      # if the full read name is "Clade_1_01_R1_001.fastq" and Clade_1_01_R2_001.fastq"
-      # then only put the portion of the file names that change to correspond to the read pairs
-      # in this example, Clade_1_01_ identify the taxons and so must not be included
-
-      r1_tail="R1.fastq"
-      r2_tail="R2.fastq"
-
-      # the output directory for your final information
-
-      outdir="$workd/$dir/phycorder-out"
+    outdir="$workd/$dir/phycorder-out"
 
 phy_loop
 
-      wait
-
-      time $phycorder_path/multi_map.sh ./basic.cfg
-
-      rm ./updated_phycorder_required_files/RAxML_bestTree.consensusFULL
-
-      rm ./updated_phycorder_required_files/extended.aln
-
-      cp $workd/$dir/phycorder-out/combine_and_infer/RAxML_bestTree.consensusFULL ./updated_phycorder_required_files/
-
-      cp $workd/$dir/phycorder-out/combine_and_infer/extended.aln ./updated_phycorder_required_files/
-
-      wait
-    fi
-
-  done
-  # END OF PHYCORDER RUN
-
-
-  rm -r ./updated_phycorder_required_files/
-
-  mkdir $maind/phycorder_results
-
-
-  TEMPFILE=/tmp/$$.tmp
-  echo 0 > $TEMPFILE
-
-  for dir in $(ls -d */ ); do
-    COUNTER=$[$(cat $TEMPFILE) + 1]
-
-    if [ "$dir" == "$first_tree_dir" ]; then
-
-      printf "skipping first first tree file"
-
-      echo "$COUNTER"
-
-      echo $COUNTER > $TEMPFILE
-
-    else
-
-
-      cp $workd/$dir/phycorder-out/combine_and_infer/RAxML_bestTree.consensusFULL $maind/phycorder_results/RAxML_bestTree.phycorder-$COUNTER-.out
-
-      cp $workd/$dir/phycorder-out/combine_and_infer/extended.aln $maind/phycorder_results/phycorder-$COUNTER-.aln
-
-      cp $workd/$dir/phycorder-out/combine_and_infer/RAxML_bipartitions.majority_rule_bootstrap_consensus $maind/phycorder_results/RAxML_bipartitions.phycorder_majority_rule-$COUNTER-.out
-
-      echo "$COUNTER"
-
-      echo $COUNTER > $TEMPFILE
-
-
-    fi
     wait
 
-  done
+    time $phycorder_path/multi_map.sh ./basic.cfg
 
-  unlink $TEMPFILE
+    wait
 
-  # GON PHYLING SECTION
+    cp $workd/$dir/phycorder-out/combine_and_infer/RAxML_bestTree.consensusFULL ./updated_phycorder_required_files/
+
+    cp $workd/$dir/phycorder-out/combine_and_infer/extended.aln ./updated_phycorder_required_files/
+
+    wait
+  else
+
+    cat <<phy_loop > basic.cfg
+
+    ## Welcome to the Phycorder config file
+    # Change the variable values to match the files and numbers you wish to use
+    # this is the path to the phycorder directory. Dont move it.
+
+    # PHYCORDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+    # the path to the previously generated alignment file
+
+    align="$workd/updated_phycorder_required_files/extended.aln"
+
+    # the path to the previously generated tree file made from the alignment file
+
+    tree="$workd/updated_phycorder_required_files/RAxML_bestTree.consensusFULL"
+
+    # the directory of paired end read pairs you belonging to taxa you wish to add to your phylogeny
+
+    read_dir="$workd/$dir"
+
+    # the number of taxa that can be added to the phylogeny at a single time
+    # should be less than the number of cores available
+    # should be balanced with the number of threads you will assign to the programs within phycorder
+
+    phycorder_runs="2"
+
+    # the number of threads you wish to make available to each phycorder run
+    # for mapping with bowtie and inference with RAxML
+
+    threads="2"
+
+    # the tail identifiers of the read pairs
+    # if the full read name is "Clade_1_01_R1_001.fastq" and Clade_1_01_R2_001.fastq"
+    # then only put the portion of the file names that change to correspond to the read pairs
+    # in this example, Clade_1_01_ identify the taxons and so must not be included
+
+    r1_tail="R1.fastq"
+    r2_tail="R2.fastq"
+
+    # the output directory for your final information
+
+    outdir="$workd/$dir/phycorder-out"
+
+phy_loop
+
+    wait
+
+    time $phycorder_path/multi_map.sh ./basic.cfg
+
+    wait
+
+    rm ./updated_phycorder_required_files/RAxML_bestTree.consensusFULL
+
+    rm ./updated_phycorder_required_files/extended.aln
+
+    cp $workd/$dir/phycorder-out/combine_and_infer/RAxML_bestTree.consensusFULL ./updated_phycorder_required_files/
+
+    cp $workd/$dir/phycorder-out/combine_and_infer/extended.aln ./updated_phycorder_required_files/
+
+    wait
+  fi
+
+done
+# END OF PHYCORDER RUN
 
 
-  cd $maind
+rm -r ./updated_phycorder_required_files/
 
-  gon_results=gon_phy_results
+mkdir $maind/phycorder_results
 
-  gon_runs=gon_phy_runs_dir
 
-  mkdir $gon_results
+TEMPFILE=/tmp/$$.tmp
+echo 0 > $TEMPFILE
 
-  mkdir $gon_runs
+for dir in $(ls -d */ ); do
+  COUNTER=$[$(cat $TEMPFILE) + 1]
 
-  TEMPFILE=/tmp/$$.tmp
-  echo 0 > $TEMPFILE
+  if [ "$dir" == "$first_tree_dir" ]; then
 
-  cd $workd
-  for dir in $(ls -d */ ); do
-    # if [ "$dir" != "$gon_results" ] || [ "$dir" != "$gon_runs" ]; then
-    if [ "$dir" == "$first_tree_dir" ]; then
+    printf "skipping first first tree file"
 
-      printf "skipping first tree file"
+    echo "$COUNTER"
 
-        # Fetch the value and increase it
-      COUNTER=$[$(cat $TEMPFILE) + 1]
+    echo $COUNTER > $TEMPFILE
 
-      cp $dir/*.fastq $maind/gon_phy_runs_dir/
+  else
 
-      echo $COUNTER > $TEMPFILE
-  #     fi
-  #
-  #   elif [ "$dir" != "$gon_results" ] || [ "$dir" != "$gon_runs" ]; then
+
+    cp $workd/$dir/phycorder-out/combine_and_infer/RAxML_bestTree.consensusFULL $maind/phycorder_results/RAxML_bestTree.phycorder-$COUNTER-.out
+
+    cp $workd/$dir/phycorder-out/combine_and_infer/extended.aln $maind/phycorder_results/phycorder-$COUNTER-.aln
+
+    cp $workd/$dir/phycorder-out/combine_and_infer/RAxML_bipartitions.majority_rule_bootstrap_consensus $maind/phycorder_results/RAxML_bipartitions.phycorder_majority_rule-$COUNTER-.out
+
+    echo "$COUNTER"
+
+    echo $COUNTER > $TEMPFILE
+
+
+  fi
+  wait
+
+done
+
+unlink $TEMPFILE
+
+# GON PHYLING SECTION
+
+
+cd $maind
+
+gon_results=gon_phy_results
+
+gon_runs=gon_phy_runs_dir
+
+mkdir $gon_results
+
+mkdir $gon_runs
+
+TEMPFILE=/tmp/$$.tmp
+echo 0 > $TEMPFILE
+
+cd $workd
+for dir in $(ls -d */ ); do
+  # if [ "$dir" != "$gon_results" ] || [ "$dir" != "$gon_runs" ]; then
+  if [ "$dir" == "$first_tree_dir" ]; then
+
+    printf "skipping first tree file"
+
+      # Fetch the value and increase it
+    COUNTER=$[$(cat $TEMPFILE) + 1]
+
+    cp $dir/*.fastq $maind/gon_phy_runs_dir/
+
+    echo $COUNTER > $TEMPFILE
+#     fi
+#
+#   elif [ "$dir" != "$gon_results" ] || [ "$dir" != "$gon_runs" ]; then
 
   else
     printf "$dir"
@@ -321,7 +325,7 @@ gon_phy_loop
 
   #   else
   #     printf "$dir"
-   $phycorder_path/gon_phyling.sh ./gon_phy_basic.cfg
+    $phycorder_path/gon_phyling.sh ./gon_phy_basic.cfg
 
     wait
 
@@ -339,66 +343,66 @@ gon_phy_loop
 
     wait
 
-    fi
-
-  done
-
-  unlink $TEMPFILE
-
-  mkdir $maind/combined_outputs
-
-  cp $maind/gon_phy_results/* $maind/combined_outputs/
-
-  cp $maind/phycorder_results/* $maind/combined_outputs/
-
-  wait
-
-  # ANALYSES SUMMARY SECTION
-
-  # produce data outputs for gon_phyling alignments
-
-  cd $maind/combined_outputs
-
-  TEMPFILE=/tmp/$$.tmp
-  echo 1 > $TEMPFILE
-  for seq_file in $(ls *.fas); do
-    COUNTER=$[$(cat $TEMPFILE) + 1]
-
-    $phycorder_path/phy_stats.py --align_file $seq_file --taxon_output_file phycorder_taxon_stats-$COUNTER-.csv --align_output_file phycorder_total_align_stats-$COUNTER-.csv
-
-    echo $COUNTER > $TEMPFILE
-  done
-
-  unlink $TEMPFILE
-
-  # produce data outputs for phycorder alignments
-
-  TEMPFILE=/tmp/$$.tmp
-  echo 1 > $TEMPFILE
-  for seq_file in $(ls *.aln); do
-    COUNTER=$[$(cat $TEMPFILE) + 1]
-
-    $phycorder_path/phy_stats.py --align_file $seq_file --taxon_output_file gon_phy_taxon_stats-$COUNTER-.csv --align_output_file gon_phy_total_align_stats-$COUNTER-.csv
-
-    echo $COUNTER > $TEMPFILE
-  done
-
-  unlink $TEMPFILE
-
-  cd $maind
-
-  # move all the valuable files into a folder that shows which run its a part of
-
-  mv ./combined_outputs ./combined_output-$REP_COUNTER
-  mv ./phycorder_runs_out ./phycorder_runs_out-$REP_COUNTER
-  mv ./phycorder_results ./phycorder_results-$REP_COUNTER
-  mv ./gon_phy_runs_dir ./gon_phy_runs_dir-$REP_COUNTER
-  mv ./gon_phy_results ./gon_phy_results-$REP_COUNTER
-
-  echo $REP_COUNTER > $REP_TEMPFILE
+  fi
 
 done
 
-unlink $REP_TEMPFILE
+unlink $TEMPFILE
+
+mkdir $maind/combined_outputs
+
+cp $maind/gon_phy_results/* $maind/combined_outputs/
+
+cp $maind/phycorder_results/* $maind/combined_outputs/
+
+wait
+
+# ANALYSES SUMMARY SECTION
+
+# produce data outputs for gon_phyling alignments
+
+cd $maind/combined_outputs
+
+TEMPFILE=/tmp/$$.tmp
+echo 1 > $TEMPFILE
+for seq_file in $(ls *.fas); do
+  COUNTER=$[$(cat $TEMPFILE) + 1]
+
+  $phycorder_path/phy_stats.py --align_file $seq_file --taxon_output_file phycorder_taxon_stats-$COUNTER-.csv --align_output_file phycorder_total_align_stats-$COUNTER-.csv
+
+  echo $COUNTER > $TEMPFILE
+done
+
+unlink $TEMPFILE
+
+# produce data outputs for phycorder alignments
+
+TEMPFILE=/tmp/$$.tmp
+echo 1 > $TEMPFILE
+for seq_file in $(ls *.aln); do
+  COUNTER=$[$(cat $TEMPFILE) + 1]
+
+  $phycorder_path/phy_stats.py --align_file $seq_file --taxon_output_file gon_phy_taxon_stats-$COUNTER-.csv --align_output_file gon_phy_total_align_stats-$COUNTER-.csv
+
+  echo $COUNTER > $TEMPFILE
+done
+
+unlink $TEMPFILE
+#
+#   cd $maind
+#
+#   # move all the valuable files into a folder that shows which run its a part of
+#
+#   mv ./combined_outputs ./combined_output-$REP_COUNTER
+#   mv ./phycorder_runs_out ./phycorder_runs_out-$REP_COUNTER
+#   mv ./phycorder_results ./phycorder_results-$REP_COUNTER
+#   mv ./gon_phy_runs_dir ./gon_phy_runs_dir-$REP_COUNTER
+#   mv ./gon_phy_results ./gon_phy_results-$REP_COUNTER
+#
+#   echo $REP_COUNTER > $REP_TEMPFILE
+#
+# done
+#
+# unlink $REP_TEMPFILE
 
 printf "Stepwise phylogenetic analyses finished"

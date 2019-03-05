f30058fe7143e8d8458a35d71405de4ac8b6af40
vortacs <jtoscanifield@ucmerced.edu>
2019-03-04 17:11:27 -0800

    made replicate.sh source the config file for the initial output dir name. this may lead to problems later

--- replicate.sh
+++ replicate.sh
@@ -2,6 +2,8 @@
 
 # Wrapper script to loop our comparison bash script
 
+source $1
+
 REP_TEMPFILE=/tmp/rep_temp.tmp
 echo 0 > $REP_TEMPFILE
 
@@ -16,7 +18,7 @@ for ((n=0;n<$2;n++)); do
   # move all the valuable files into a folder that shows which run its a part of
 
   mv ./combined_outputs ./combined_output-$REP_COUNTER
-  mv ./phycorder_runs_out ./phycorder_runs_out-$REP_COUNTER
+  mv $outdir ./$outdir-$REP_COUNTER
   mv ./phycorder_results ./phycorder_results-$REP_COUNTER
   mv ./gon_phy_runs_dir ./gon_phy_runs_dir-$REP_COUNTER
   mv ./gon_phy_results ./gon_phy_results-$REP_COUNTER

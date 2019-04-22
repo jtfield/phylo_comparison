4b51cea2d9a6a3f2077eb0c86724cdacf4015e3b
vortacs <jtoscanifield@ucmerced.edu>
2019-04-22 12:01:07 -0700

    updated functionality to split a noraml fasta file as well as a phycorder output

--- alignment_splitter.py
+++ alignment_splitter.py
@@ -8,6 +8,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--align_file')
     parser.add_argument('--taxon_num')
+    parser.add_argument('-p', action='store_true', help='parsing and splitting of phycorder produced alignment file')
+    parser.add_argument('-f', action='store_true', help='parsing and splitting of a genome fasta file')
     return parser.parse_args()
 
 def main():
@@ -19,35 +21,54 @@ def main():
 
     split_align = read_file.split('>')
 
-    for seq in split_align[1:]:
-        split_name_and_seq = seq.split('\n')
-        taxon_name = split_name_and_seq[0]
-        name_split = taxon_name.split('_')
-        taxon_number = name_split[1]
-        if taxon_number == args.taxon_num:
-            print(taxon_number)
-            seq = split_name_and_seq[1]
+    if args.p == True:
 
-            # make a complete alignment
+        for seq in split_align[1:]:
+            split_name_and_seq = seq.split('\n')
+            taxon_name = split_name_and_seq[0]
+            name_split = taxon_name.split('_')
+            taxon_number = name_split[1]
+            if taxon_number == args.taxon_num:
+                print(taxon_number)
+                seq = split_name_and_seq[1]
 
-            # Split each seq into seperate parts
-            split_seq = seq.split('n'*100)
+                # make a complete alignment
 
-            # start counting each part of the sequence and open files to get those sequences
-            loci_count = 0
-            for small_seq in split_seq:
-                loci_count+=1
-                loci_file = open(taxon_name + "_loci_" + str(loci_count) + "chunk.fas", "w")
-                loci_file.write('>')
-                loci_file.write(taxon_name)
-                loci_file.write('_' + str(loci_count))
-                loci_file.write('\n')
-                loci_file.write(small_seq)
-                loci_file.write('\n')
+                # Split each seq into seperate parts
+                split_seq = seq.split('n'*100)
 
-                loci_file.close()
+                # start counting each part of the sequence and open files to get those sequences
+                loci_count = 0
+                for small_seq in split_seq:
+                    loci_count+=1
+                    loci_file = open(taxon_name + "_loci_" + str(loci_count) + "chunk.fas", "w")
+                    loci_file.write('>')
+                    loci_file.write(taxon_name)
+                    loci_file.write('_' + str(loci_count))
+                    loci_file.write('\n')
+                    loci_file.write(small_seq)
+                    loci_file.write('\n')
+
+                    loci_file.close()
 
 
+    elif args.f == True:
+        for seq in split_align[1:]:
+            split_name_and_seq = seq.split('\n')
+            seq.split('\n', 1)[1]
+            # print(seq)
+
+            taxon_name = split_name_and_seq[0]
+            # seq = split_name_and_seq[1]
+            #
+            loci_file = open(taxon_name + "_chunk.fas", "w")
+            loci_file.write('>')
+            # loci_file.write(taxon_name)
+            # loci_file.write('\n')
+            loci_file.write(seq)
+            loci_file.write('\n')
+            #
+            loci_file.close()
 
 
 

bbf7c9330176d6d99be5dfa05f0a2d9113f834ef
vortacs <jtoscanifield@ucmerced.edu>
2019-10-21 19:28:39 -0700

    added full gon_phyling

--- basecall_comparison.sh
+++ basecall_comparison.sh
@@ -19,11 +19,13 @@ start_dir=start_tree_dir
 update_dir=update_alignment_dir
 loci_blast_indexes=loci_blast_indexes_dir
 truncated_tree=phycorder_low_loci_dir
+gon_phy=gon_phyling_dir
 
 mkdir $start_dir
 mkdir $truncated_tree
 mkdir $update_dir
 mkdir $loci_blast_indexes
+mkdir $gon_phy
 
 # link files for the starting tree into a seperate directory
 # seperate directory for both the complete run and the single addition of a taxon run
@@ -36,6 +38,17 @@ done
 for i in $(cat $outdir"/"update_read_list.txt); do
   ln -s "${read_dir}"/$i $update_dir
   ln -s "${read_dir}"/${i%$r1_tail}$r2_tail $update_dir
+done
+
+for i in $(cat $outdir"/"start_tree_files.txt); do
+  ln -s "${read_dir}"/$i $gon_phy
+  ln -s "${read_dir}"/${i%$r1_tail}$r2_tail $gon_phy
+done
+
+# link files for updating the starting alignment to a seperate directory
+for i in $(cat $outdir"/"update_read_list.txt); do
+  ln -s "${read_dir}"/$i $gon_phy
+  ln -s "${read_dir}"/${i%$r1_tail}$r2_tail $gon_phy
 done
 
 
@@ -102,9 +115,47 @@ for i in $(cat $outdir"/"$loci_blast_indexes"/"loci_for_use.txt); do
 done
 
 # COMBINE SELECTED LOCI INTO SINGLE FASTA FILE FOR TRUNCATED PHYCORDER RUN
-$phycorder_path/locus_combiner.py --msa_folder $truncated_tree --out_file phycord_base.fasta --position_dict_file pos_file.txt --suffix .fasta
+$phycorder_path/locus_combiner.py --msa_folder $outdir/$truncated_tree/ --out_file $outdir/$truncated_tree/phycord_base.fasta --position_dict_file $outdir/$truncated_tree/pos_file.txt --suffix .fasta
 
+# RUN GON_PHYLING ON READS FOR ALL TAXA IN LOCUS MODE
+if [ $gon_phy_alignments == "OFF" ]; then
 
+# create a config file on the fly with current settings
+  cat <<EOF > gon_phy_assembly.cfg
+  ## Config file for gon_phyling pipeline
+  # change the values of the variables to control the pipeline
+  # the variables are structured like this:
+  # variable_name="value_of_the_variable"
+  # value of the variable can be a path to a file or directory of files
+  # Path to the reference genome required for Parsnp
+  ref_genome="$ref_genome"
+  # Path to the directory of reads for assembly
+  read_dir="$outdir/$gon_phy"
+  # number of runs for gon_phyling to commence in parallel
+  gon_phy_runs="$gon_phy_runs"
+  # number of threads for Spades assembly and RAxML inference
+  threads="$THREADS"
+  # File stubs required to assemble paired end reads
+  r1_tail="$r1_tail"
+  r2_tail="$r2_tail"
+  #bootstrapping
+  bootstrapping="OFF"
+  # type of output
+  # this setting determines if you output a single, concatenated loci MSA fasta file,
+  # multiple single locus MSA fasta files
+  # or a single nexus file that includes the start and stop locations of each loci
+  # EXAMPLE: output_type="LOCI"
+  # EXAMPLE: output_type="LOCUS"
+  # EXAMPLE: output_type="NEXUS" ~~~~~~NOT SUPPORTED YET~~~~~~
+  output_type="$output_type"
+  # output location for the positional dict file if enabled
+  #loci_positions="$loci_positions"
+  loci_positions="$outdir/gon_phy_run_positions.txt"
+EOF
+
+fi
+
+time $phycorder_path/gon_phyling.sh $outdir/gon_phy_assembly.cfg
 
 
 

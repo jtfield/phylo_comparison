cfed1251f10d978b1b4abcacf9c39ff8d1d6d178
vortacs <jtoscanifield@ucmerced.edu>
2019-11-06 20:18:37 -0800

    reads csv now, also uses random numbers to select loci

--- select_ten.py
+++ select_ten.py
@@ -36,7 +36,8 @@ def main():
             locus_num = row['locus_position_number']
             locus_count = locus_num
     print(locus_count)
-
+    
+    loci_to_use_names = []
     loci_to_use_list = []
     if locus_count > 1:
 
@@ -52,42 +53,22 @@ def main():
 
 
 
-    #with open(args.position_csv_file) as csvfile:
+    with open(args.position_csv_file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            #row['locus_position_number'], row['locus_file_name'], row['locus_length']
+            if int(row['locus_position_number']) in loci_to_use_list:
+                loci_to_use_names.append(row['locus_file_name'])
+
+    print(loci_to_use_names)
 
 
 
-    #json_data = open(args.position_dict_file,'r')
-    #read_dict = json_data.read()
-    #pos_dict = json.loads(read_dict)
-    #print(pos_dict)
-
-#    loci_for_use = []
-#    loci_count = 0
-#    print(args.num_loci)
-#    print("ABOVE IS SELECT_TEN_LOCI_CUTOFF")
-#    for pos, locus_name_and_len in pos_dict.items():
-#        for leng, name in locus_name_and_len.items():
-#            #print(type(leng))
-#            print(leng)
-#            print(type(args.num_loci))
-#            print(type(leng))
-#            print(name)
-#            print(loci_count)
-#            print("##########################################################")
-#            if int(leng) >= 2000:
-#                if loci_count < int(args.num_loci):
-#                    if name in msa_list:
-#                        print("~~~~~~~~~~~~~~~~~~~~~~~~LOCI SELECTED~~~~~~~~~~~~~~~~~~")
-#                        loci_for_use.append(str(name))
-#                        loci_count+=1
-#     
-#    print(loci_for_use)
-#
-#    open_output = open(args.out_file,"w")
-#    for name in loci_for_use:
-#        open_output.write(name)
-#        open_output.write("\n")
-#    open_output.close()
+    open_output = open(args.out_file,"w")
+    for name in loci_to_use_names:
+        open_output.write(name)
+        open_output.write("\n")
+    open_output.close()
 
 if __name__ == '__main__':
     main()

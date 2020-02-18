f1e58ab63652f511cf4762d57669eb750a30a7a8
vortacs <jtoscanifield@ucmerced.edu>
2020-02-18 13:29:27 -0800

    working through automatic file matching for blast analysis

--- multi_method_basecall_check/multi_method_basecall.sh
+++ multi_method_basecall_check/multi_method_basecall.sh
@@ -33,6 +33,8 @@ done
 
 #CALL GON_PHYLING ON ALL TAXA READS
 $rapup_path/gon_phyling.sh -d $gon_phy_pwd -1 $r1_tail -2 $r2_tail -o LOCUS -l $loci_positions
+
+sed -i -e '/.ref//g' $gon_phy_pwd/trimmed_reads/spades_output/genomes_for_parsnp/alignment_fixing/combo.fas
 printf "\nGON_PHYLING STAGE COMPLETE\n"
 #SET UP REFERENCE BY PULLING A SINGLE SEQUENCE FROM THE GON_PHYLING RUN ALIGNMENT
 cd $outdir
@@ -77,5 +79,47 @@ chmod +x $outdir/runme.sh
 
 $outdir/runme.sh
 
+#BEGIN SEPARATION OF LOCI IF NECESSARY AND ALIGNMENT OF CONSTRUCTED LOCI TO THE ORIGINAL GENOMES THAT PRODUCED THE READS
 
+# CONSTRUCT BLAST INDEXES
+mkdir $outdir/$loci_blast_indexes
+
+for i in $(ls $reference_dir);
+do
+	printf "\n$reference_dir/$i\n"
+	ln -s $reference_dir/$i $outdir/$loci_blast_indexes/$i
+	makeblastdb -in $outdir/$loci_blast_indexes/$i -dbtype nucl -parse_seqids
+
+done
+
+mkdir $outdir/$rapup_basecall
+mkdir $outdir/$snippy_basecall
+mkdir $outdir/$gon_phy_basecall
+mkdir $outdir/$rapup_basecall/blast_results
+mkdir $outdir/$snippy_basecall/blast_results
+mkdir $outdir/$gon_phy_basecall/blast_results
+
+#SPLIT EACH LOCUS INTO A SEPARATE ALIGNMENT FILE
+
+#TODO: return name of reference for the snippy output
+
+$rapup_path/locus_position_identifier.py --out_file_dir $outdir/$rapup_basecall/sep_loci --position_csv_file $loci_positions --concatenated_fasta $outdir/rapup_run/combine_and_infer/extended.aln
+
+$rapup_path/locus_position_identifier.py --out_file_dir $outdir/$snippy_basecall/sep_loci --position_csv_file $loci_positions --concatenated_fasta $outdir/core.full.aln
+
+$rapup_path/locus_position_identifier.py --out_file_dir $outdir/$gon_phy_basecall/sep_loci --position_csv_file $loci_positions --concatenated_fasta $outdir/trimmed_reads/spades_output/genomes_for_parsnp/alignment_fixing/combo.fas
+
+mkdir $outdir/$rapup_basecall/sep_loci/individual_tax
+mkdir $outdir/$snippy_basecall/sep_loci/individual_tax
+mkdir $outdir/$gon_phy_basecall/sep_loci/individual_tax
+
+for j in $(ls $outdir/$gon_phy_basecall/sep_loci/*.fasta);
+do
+	for i in $(grep ">" $j | sed -e 's/>//g');
+	do
+		printf "\n$j\n$i\n"
+		msa_file=$(basename $j)
+		$rapup_path/ref_producer.py -s --align_file $j --out_file $outdir/$gon_phy_basecall/sep_loci/individual_tax/single_tax_$msa_file_$i --ref_select $i
+	done
+done
 

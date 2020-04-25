1dc93af628a634ddc9c5434a89cd6ba920cb2d08
vortacs <jtoscanifield@ucmerced.edu>
2020-04-25 15:14:56 -0700

    functionalized main computations and added handling of sequence orientations

--- multi_method_basecall_check/alignment_checker.py
+++ multi_method_basecall_check/alignment_checker.py
@@ -27,6 +27,40 @@ def check_alignment(list_of_paired_nucs):
 
     return identical_nucs
 
+def perform_alignment_check(short, long_seq):
+    output = []
+    identical_bases_by_position = 0
+    identical_bases = 0
+    best_seq = ''
+    added_gaps_on_front = 0
+    #add_gaps_short = ((added_gaps_on_front * '-') + shorter)
+    #extended_short_length = len(shorter) + added_gaps_on_front
+    #fixed_shorter
+    while added_gaps_on_front + len(short) < int(len(long_seq)):
+    #while added_gaps_on_front + len(shorter) < 500:
+        #add_gaps_short = ((added_gaps_on_front * '-') + shorter)
+        split_short = split_seq(short)
+        split_long = split_seq(long_seq[added_gaps_on_front : added_gaps_on_front + len(short)])
+        added_gaps_on_front+=1
+        #print(split_long)
+        #print(split_short)
+        #print("waffle")
+
+
+        combined_positions = list(map(list, zip(split_long, split_short)))
+        check_identity = check_alignment(combined_positions)
+        if check_identity > identical_bases:
+            print(check_identity)
+            identical_bases = check_identity
+            identical_bases_by_position = added_gaps_on_front
+            best_seq = split_short
+
+    output.append(identical_bases_by_position)
+    output.append(identical_bases)
+    output.append(best_seq)
+
+    return output
+
 
 def main():
     args = parse_args()
@@ -75,68 +109,70 @@ def main():
     short_reverse = main_short[::-1]
     short_rev_comp = main_short.reverse_complement()
     
-    print(main_short)
-    print(short_comp)
-    print(short_reverse)
-    print(short_rev_comp)
+    #print(main_short)
+    #print(short_comp)
+    #print(short_reverse)
+    #print(short_rev_comp)
 
 
     identical_bases_by_position = 0
     identical_bases = 0
-    #split_short = split_seq(shorter)
-#    for num in range(0, len(longer)):
-#        #longer_segment = longer[num:len(shorter)]
-#        longer_segment = longer[num:num + len(shorter)]
-#        if len(longer_segment) >= len(shorter):
-#        
-#            split_long = split_seq(longer_segment)
-#            #print(len(split_long))
-#            #print(len(longer))
-#            
-#            # make sure that the split subsequence of the longer sequence is not shorter than the shorter sequence (ugh)
-#            assert len(split_long) >= len(shorter)
-#           
-#            #zip both lists together by elements
-#            combined_positions = list(map(list, zip(split_long, split_short)))
-#            #print(combined_positions)
-#            check_identity = check_alignment(combined_positions)
-#            #print(check_identity)
-#
-#            if check_identity > identical_bases:
-#                identical_bases = check_identity
-#                identical_bases_by_position = num
+
+    analyzed_main_short = perform_alignment_check(main_short, longer) 
+    #print(analyzed_main_short[0])
+    #print(analyzed_main_short[1])
+    analyzed_short_comp = perform_alignment_check(short_comp, longer)
+    analyzed_reverse = perform_alignment_check(short_reverse, longer)
+    analyzed_rev_comp = perform_alignment_check(short_rev_comp, longer)
+
+    all_seqs_align_scores = [analyzed_main_short[1], analyzed_short_comp[1], analyzed_reverse[1], analyzed_rev_comp[1]]
 
     best_seq = ''
-    added_gaps_on_front = 0
-    #add_gaps_short = ((added_gaps_on_front * '-') + shorter)
-    #extended_short_length = len(shorter) + added_gaps_on_front
-    #fixed_shorter
-    while added_gaps_on_front + len(shorter) < int(len(longer)):
-    #while added_gaps_on_front + len(shorter) < 500:
-        #add_gaps_short = ((added_gaps_on_front * '-') + shorter)
-        split_short = split_seq(shorter)
-        split_long = split_seq(longer[added_gaps_on_front : added_gaps_on_front + len(shorter)])
-        added_gaps_on_front+=1
-        #print(split_long)
-        #print(split_short)
-        print("waffle")
+    best_score = max(all_seqs_align_scores)
+    if best_score == all_seqs_align_scores[0]:
+        best_seq = analyzed_main_short
+    elif best_score == all_seqs_align_scores[1]:
+        best_seq = analyzed_short_comp
+    elif best_score == all_seqs_align_scores[2]:
+        best_seq = analyzed_reverse
+    elif best_score == all_seqs_align_scores[3]:
+        best_seq = analyzed_rev_comp
 
+    #print(best_seq) 
 
-        combined_positions = list(map(list, zip(split_long, split_short)))
-        check_identity = check_alignment(combined_positions)
-        if check_identity > identical_bases:
-            print(check_identity)
-            identical_bases = check_identity
-            identical_bases_by_position = added_gaps_on_front
-            best_seq = split_short
+#    best_seq = ''
+#    added_gaps_on_front = 0
+#    #add_gaps_short = ((added_gaps_on_front * '-') + shorter)
+#    #extended_short_length = len(shorter) + added_gaps_on_front
+#    #fixed_shorter
+#    while added_gaps_on_front + len(shorter) < int(len(longer)):
+#    #while added_gaps_on_front + len(shorter) < 500:
+#        #add_gaps_short = ((added_gaps_on_front * '-') + shorter)
+#        split_short = split_seq(shorter)
+#        split_long = split_seq(longer[added_gaps_on_front : added_gaps_on_front + len(shorter)])
+#        added_gaps_on_front+=1
+#        #print(split_long)
+#        #print(split_short)
+#        #print("waffle")
+#
+#
+#        combined_positions = list(map(list, zip(split_long, split_short)))
+#        check_identity = check_alignment(combined_positions)
+#        if check_identity > identical_bases:
+#            print(check_identity)
+#            identical_bases = check_identity
+#            identical_bases_by_position = added_gaps_on_front
+#            best_seq = split_short
         
 
 
 
     identical_bases_by_position = identical_bases_by_position - 1
-    print(identical_bases)
-    print(identical_bases_by_position)
+    #print(identical_bases)
+    #print(identical_bases_by_position)
     #fixed_short = ''.join(split_short)
+
+    fixed_short = ''.join(best_seq[2])
 
     output_file = open('test_output_align.fasta','w')
     output_file.write(longer_label)
@@ -145,7 +181,8 @@ def main():
     output_file.write('\n')
     output_file.write(shorter_label)
     output_file.write('\n')
-    output_file.write((identical_bases_by_position * '-') + shorter)
+    output_file.write(((best_seq[0] - 1) * '-') + fixed_short)
+    #output_file.write((identical_bases_by_position * '-') + shorter)
     #output_file.write((27 * '-') + shorter)
     output_file.close()
 

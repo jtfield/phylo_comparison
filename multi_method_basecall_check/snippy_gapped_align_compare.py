f2819e1466a2f4379c549da7bffd25d65970beaa
vortacs <jtoscanifield@ucmerced.edu>
2020-05-16 13:39:42 -0700

    programs for assessing snippy output basecalls, both the consensus sequence with variants and the sequences from the MSA

--- multi_method_basecall_check/snippy_gapped_align_compare.py
+++ multi_method_basecall_check/snippy_gapped_align_compare.py
@@ -3,8 +3,8 @@ import argparse
 import os
 import re
 import multiprocessing as mp
-from Bio.Seq import Seq
-from Bio.Alphabet import generic_dna
+#from Bio.Seq import Seq
+#from Bio.Alphabet import generic_dna
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -12,11 +12,10 @@ def parse_args():
     #parser.add_argument('--align_2')
     #parser.add_argument('--align_3')
     #parser.add_argument('--align_4')
-    parser.add_argument('--output_stub', nargs='?', type=str, default="NONE")
+    #parser.add_argument('--output_stub', nargs='?', type=str, default="NONE")
     #parser.add_argument('--output_miscalls', nargs='?', type=str, default="NONE")
     #parser.add_argument('--orientation', nargs='?', type=str, default="NONE")
     return parser.parse_args()
-
 
 def alignment_fixer(read_file):
     split_seqs = read_file.split('>')
@@ -32,7 +31,7 @@ def alignment_fixer(read_file):
             #print(split_seq_and_name)
             name = split_seq_and_name[0]
             seq = split_seq_and_name[1]
-
+        
             fixed_seq = seq.replace('\n','')
             if seq_count == 1:
                 seq_1.append(name)
@@ -51,15 +50,14 @@ def nuc_counter(sequence):
     for nuc in sequence[1]:
         if nuc != '-':
             nuc_count+=1
-
+    
     return nuc_count
-
 
 def trim_gaps(short_seq):
     output = []
     leading_gaps = 0
     trailing_gaps = 0
-
+    
     leading_gap_match = "^(-*)\w"
     trailing_gap_match = "\w(-*)$"
     compile_leading_match = re.compile(leading_gap_match)
@@ -72,7 +70,7 @@ def trim_gaps(short_seq):
         #print(find_leading)
         leading_gaps = len(find_leading[0])
         print(leading_gaps)
-
+    
     if compile_trailing_match:
         #print("found trailing")
         #print(find_trailing)
@@ -81,25 +79,23 @@ def trim_gaps(short_seq):
 
     output.append(leading_gaps)
     output.append(trailing_gaps)
-
+    
     return output
-
+    
 def check_alignment(list_of_paired_nucs):
     identical_nucs = 0
     non_identical_nucs = 0
     nuc_set = []
-    non_identical_positions = []
-    for num, pair in enumerate(list_of_paired_nucs):
+    for pair in list_of_paired_nucs:
 
         assert len(pair) == 2
         if pair[0].upper() == pair[1].upper():
             identical_nucs+=1
         elif pair[0].upper() != pair[1].upper():
             non_identical_nucs+=1
-            non_identical_positions.append(num)
     nuc_set.append(identical_nucs)
     nuc_set.append(non_identical_nucs)
-    print(non_identical_positions)
+
     #return identical_nucs
     return nuc_set
 
@@ -120,10 +116,10 @@ def comparison(list_of_list_of_seqs):
 
     count_nucs_1 = nuc_counter(seq_1)
     count_nucs_2 = nuc_counter(seq_2)
-
+    
     #print(count_nucs_1)
     #print(count_nucs_2)
-
+    
     nuc_lens = [count_nucs_1, count_nucs_2]
     small_seq = min(nuc_lens)
 
@@ -139,48 +135,27 @@ def comparison(list_of_list_of_seqs):
 
     #print(shorter)
     #print(longer)
-
+    
     shorter_seq = shorter[1]
     longer_seq = longer[1]
-
-    #print("waffle")
-    #get_gaps = trim_gaps(shorter_seq)
-    #print(get_gaps)
-    #print("waffle")
     
-    #if shorter_seq.startswith('-') or shorter_seq.startswith('N') or longer_seq.startswith('-') or longer_seq.startswith('N'):
-    #    get_gaps = trim_gaps(shorter_seq)
+    get_gaps = trim_gaps(shorter_seq) 
+    #print(get_gaps)
 
+    trimmed_shorter = shorter_seq[get_gaps[0]:-get_gaps[1]]
+    #print(trimmed_shorter)
 
-    combined_positions = list(map(list, zip(longer_seq, shorter_seq)))
+    trimmed_longer = longer_seq[get_gaps[0]:-get_gaps[1]]
+    
+    split_trimmed_short = list(trimmed_shorter)
+    split_trimmed_long = list(trimmed_longer)
+
+    combined_positions = list(map(list, zip(split_trimmed_long, split_trimmed_short)))
     analyze_alignment = check_alignment(combined_positions)
 
     print(analyze_alignment)
-
+    
     return analyze_alignment
-
-#    elif get_gaps != 0:
-#        trimmed_shorter = shorter_seq[get_gaps[0]:-get_gaps[1]]
-#        #print(trimmed_shorter)
-#
-#        trimmed_longer = longer_seq[get_gaps[0]:-get_gaps[1]]
-#
-#        split_trimmed_short = list(trimmed_shorter)
-#        split_trimmed_long = list(trimmed_longer)
-#
-#        #print(split_trimmed_short)
-#        #print(split_trimmed_long)
-#
-#        combined_positions = list(map(list, zip(split_trimmed_long, split_trimmed_short)))
-#        analyze_alignment = check_alignment(combined_positions)
-#
-#        print(analyze_alignment)
-#
-#        return analyze_alignment
-
-
-
-
 
 def main():
     args = parse_args()
@@ -188,14 +163,115 @@ def main():
     #pool = mp.Pool(mp.cpu_count())
     print("Number of processors: ", mp.cpu_count())
 
+
     align_1 = open(args.align_1,'r').read()
+    #align_2 = open(args.align_2,'r').read()
+    #align_3 = open(args.align_3,'r').read()
+    #align_4 = open(args.align_4,'r').read()
 
     parse_align_1 = alignment_fixer(align_1)
     #print(parse_align_1)
+    #parse_align_2 = alignment_fixer(align_2)
+    #parse_align_3 = alignment_fixer(align_3)
+    #parse_align_4 = alignment_fixer(align_4)
 
     compare_seqs_1 = comparison(parse_align_1)
-
+    #compare_seqs_2 = comparison(parse_align_2)
+    #compare_seqs_3 = comparison(parse_align_3)
+    #compare_seqs_4 = comparison(parse_align_4)
     print("align 1", compare_seqs_1)
+    #print("align 2", compare_seqs_2)
+    #print("align 3", compare_seqs_3)
+    #print("align 4", compare_seqs_4)
+
+    #compare_identical_nucs = [compare_seqs_1[0], compare_seqs_2[0], compare_seqs_3[0], compare_seqs_4[0]]
+    #best_align = max(compare_identical_nucs)
+
+#    output_file = open(args.output_stub, 'w')
+#    
+#    if best_align == compare_seqs_1[0]:
+#        output_file.write(args.align_1)
+#        output_file.write("\n")
+#        output_file.write(str(compare_seqs_1[0]))
+#        output_file.write("\n")
+#        output_file.write(str(compare_seqs_1[1]))
+
+#    elif best_align == compare_seqs_2[0]:
+#        output_file.write(args.align_2)
+#        output_file.write("\n")
+#        output_file.write(str(compare_seqs_2[0]))
+#        output_file.write("\n")
+#        output_file.write(str(compare_seqs_2[1]))
+#
+#    elif best_align == compare_seqs_3[0]:
+#        output_file.write(args.align_3)
+#        output_file.write("\n")
+#        output_file.write(str(compare_seqs_3[0]))
+#        output_file.write("\n")
+#        output_file.write(str(compare_seqs_3[1]))
+#
+#    elif best_align == compare_seqs_4[0]:
+#        output_file.write(args.align_4)
+#        output_file.write("\n")
+#        output_file.write(str(compare_seqs_4[0]))
+#        output_file.write("\n")
+#        output_file.write(str(compare_seqs_4[1]))
+
+
+    #align_1_split = align_1.split('\n', 1)
+    #align_2_split = align_2.split('\n', 1)
+    #align_3_split = align_3.split('\n', 1)
+    #align_4_split = align_4.split('\n', 1)
+
+    #label = align_1_split[0]
+    #seq = align_1_split[1]
+
+    #len_align_1 = len(align_1_split[1])
+    #len_align_2 = len(align_2_split[1])
+
+    
+    #shorter = seq.replace('\n','')
+    #longer = longer.replace('\n','')
+    
+    #print("Longer sequence length: ", len(longer))
+    #print("Shorter sequence length: ", len(shorter))
+    
+    #print(len(shorter))
+
+
+
+    #main_short = Seq(shorter, generic_dna)
+    #short_comp = main_short.complement()
+    #short_reverse = main_short[::-1]
+    #short_rev_comp = main_short.reverse_complement()
+   
+   
+   #Produce reverse sequence
+    #output_file = open(args.output_align_stub + "-reverse.fasta", 'w')
+    #output_file.write(label)
+    #output_file.write('\n')
+    #output_file.write(str(short_reverse))
+    #output_file.close()
+
+
+   #Produce compliment sequence
+    #output_file = open(args.output_align_stub + "-complement.fasta", 'w')
+    #output_file.write(label)
+    #output_file.write('\n')
+    #output_file.write(str(short_comp))
+    #output_file.close()
+
+
+    #Produce reverse complement sequence
+    #output_file = open(args.output_align_stub + "-reverse_complement.fasta", 'w')
+    #output_file.write(label)
+    #output_file.write('\n')
+    #output_file.write(str(short_rev_comp))
+    #output_file.close()
+
+
+
+    #print("Number of processors: ", mp.cpu_count())
 
 if __name__ == '__main__':
     main()
